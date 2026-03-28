5fa4c4d91902ea38646d4bf0495da0a736df7171
Pablo Rizo <prozasoconw4@gmail.com>
2026-03-28 11:40:17 -0600

    FEAT: CAMBIO SCRIPT

--- run_test.sh
+++ run_test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -e # Detener el script si algo falla
+
+VENV_PATH="./.venv"
+
 echo "Creando entorno virtual..."
-python3 -m venv .venv
+python3 -m venv $VENV_PATH
 
-echo "Activando el entorno virtual..."
-source .venv/bin/activate
-
-echo "instalando dependencias..."
-pip install -r requirements.txt
+# Usamos la ruta absoluta al python del venv para instalar y ejecutar
+echo "Instalando dependencias..."
+$VENV_PATH/bin/python3 -m pip install --upgrade pip
+$VENV_PATH/bin/python3 -m pip install -r requirements.txt
 
 echo "Ejecutando pruebas con pytest..."
-pytest test/ --junitxml=reports/test_results.xml --html=reports/test_report.html --self-contained-html
+$VENV_PATH/bin/pytest test/ --junitxml=reports/test_results.xml --html=reports/test_report.html --self-contained-html
 
-echo "Pruebas finalizadas. resultados guardados en reports/test_results.xml y reports/test_report.html"
+echo "Proceso completado."
