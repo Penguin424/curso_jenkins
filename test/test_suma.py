3f95eb5283860be302e4dfc25834ce13b666fc00
Pablo Rizo <prozasoconw4@gmail.com>
2026-03-28 11:50:23 -0600

    FEAT: QUITADO ERROES

--- test/test_suma.py
+++ test/test_suma.py
@@ -10,6 +10,6 @@ def test_suma():
     assert suma(-1, 1) == 0
     assert suma(0, 0) == 0
 
-def test_suma_fail():
-    assert suma(1,2) == 4
+# def test_suma_fail():
+#     assert suma(1,2) == 4
     
